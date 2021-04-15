9a95ae5317f6df7fbfa635530fc5005365de6bf5
anh-nhat <Git15061992>
2021-04-15 22:43:45 +0700

    new vue3-tutorial

---
+++ vue3-turtorial/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers: </strong> {{ followers }}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_AnhNhat',
+        firsName: 'Revival',
+        lastName: 'Ginta',
+        email: 'nhat1506@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firsName} ${this.user.lastName}`;
+    }
+  },
+  methods:{
+    followUser(){
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser()
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+}
+</style>
