ce7d8767a407be5604c4794aa75ecc3a43483e28
anhnhat1506 <nhat15061992@gmail.com>
2021-04-16 17:17:29 +0700

    TwootItemv3

--- vue3-turtorial/src/App.vue
+++ vue3-turtorial/src/App.vue
@@ -1,50 +1,16 @@
 <template>
   <div id="app">
-    @{{ user.username }} - {{ fullName }}
-    <strong>Followers: </strong> {{ followers }}
-    <button @click="followUser">
-      Follow
-    </button>
+    <UserProfile />
   </div>
 </template>
 
 <script>
-
+import UserProfile from './components/UserProfile';
 export default {
   name: 'App',
-  data(){
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: '_AnhNhat',
-        firsName: 'Revival',
-        lastName: 'Ginta',
-        email: 'nhat1506@gmail.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch:{
-    followers(newFollowerCount, oldFollowerCount){
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`)
-      }
-    }
-  },
-  computed: {
-    fullName(){
-      return `${this.user.firsName} ${this.user.lastName}`;
-    }
-  },
-  methods:{
-    followUser(){
-      this.followers++
-    }
-  },
-  mounted() {
-    this.followUser()
-  },
+  components: {
+    UserProfile,
+  }
 }
 </script>
 
@@ -53,9 +19,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  display: flex;
-  flex-direction: column;
+  min-height: 100vh;
+  background-color: #F3F5FA;
 }
 </style>
